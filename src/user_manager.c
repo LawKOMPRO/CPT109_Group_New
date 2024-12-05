0744a404147f8462aae512ad6af2268dbf016f21
dggfdc <2042016556@qq.com>
2024-12-05 20:29:28 +0800

    Update user_manager.c

--- src/user_manager.c
+++ src/user_manager.c
@@ -1,7 +1,36 @@
-// #include <stdio.h>
-// #include <user_manager.h>
-//
-// void hello()
-// {
-//     printf("hello");
-// }
+#include "user_manager.h"
+#include <stdbool.h>
+
+
+void user_manager () {
+    char choice;
+    bool flag = true;
+    while (1) {
+        printf("User Management\n");
+        printf("1. Add new user\n");
+        printf("2. Edit detailed user information\n");
+        printf("3. View past contributions and activities\n");
+        printf("4. Exit\n");
+        printf("Select an option:");
+        scanf("%s", &choice);
+
+        switch (choice) {
+            case '1':
+                registerNewAccount();
+            break;
+            case '2':
+                editAccountDetails();
+            break;
+            case '3':
+                viewContributions();
+            break;
+            case '4':
+                exit(0);
+            // case '\0':
+            //     break;
+            default:
+                printf("invalid Selection.\n\n");
+        }
+    }
+    return;
+}
